2a1d4821d6a78cda01ba1bd1f5a0587c13330a16
Richard Ettema <richard.ettema@aegis.net>
2026-08-05 16:06:57 -0400

    Updated resource data SQL scripts

--- database/mysql/resourcemetadata_insert.sql
+++ database/mysql/resourcemetadata_insert.sql
@@ -1,12 +1,15 @@
 /*
--- Insert default resourcemetadata
+-- Query: SELECT * FROM fastconsentr4.resourcemetadata
+LIMIT 0, 2000
+
+-- Date: 2026-08-05 15:55
 */
 INSERT INTO fastconsentr4.resourcemetadata (id,resourceJoinId,paramName,paramType,paramValue,systemValue,codeValue,textValue,paramValueU,textValueU) VALUES
 (1,1,'_id','TOKEN','FASTConsentSubscriptionTopic',NULL,NULL,NULL,'FASTCONSENTSUBSCRIPTIONTOPIC',NULL),
-(2,1,'_lastUpdated','DATE','20250627150128',NULL,'20250627110128',NULL,'20250627150128',NULL),
+(2,1,'_lastUpdated','DATE','20260805193934',NULL,'20260805153934',NULL,'20260805193934',NULL),
 (3,1,'derived-or-self','URI','http://hl7.org/fhir/us/consent-management/SubscriptionTopic/FASTConsentSubscriptionTopic',NULL,NULL,NULL,'HTTP://HL7.ORG/FHIR/US/CONSENT-MANAGEMENT/SUBSCRIPTIONTOPIC/FASTCONSENTSUBSCRIPTIONTOPIC',NULL),
 (4,1,'url','URI','http://hl7.org/fhir/us/consent-management/SubscriptionTopic/FASTConsentSubscriptionTopic',NULL,NULL,NULL,'HTTP://HL7.ORG/FHIR/US/CONSENT-MANAGEMENT/SUBSCRIPTIONTOPIC/FASTCONSENTSUBSCRIPTIONTOPIC',NULL),
-(5,1,'version','TOKEN','1.0.0',NULL,NULL,NULL,'1.0.0',NULL),
+(5,1,'version','TOKEN','1.0.0-preview',NULL,NULL,NULL,'1.0.0-PREVIEW',NULL),
 (6,1,'title','STRING','FAST Consent Subscription Topic',NULL,NULL,NULL,'FAST CONSENT SUBSCRIPTION TOPIC',NULL),
 (7,1,'status','TOKEN','active',NULL,NULL,NULL,'ACTIVE',NULL),
 (8,1,'date','DATE','20250301050000',NULL,'20250301000000',NULL,'20250301050000',NULL);
